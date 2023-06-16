6c9adfb225fcb98556b37a9a44b864bd7b8132c9
Kevin Duhamel Hayes <kevinduhamelh@gmail.com>
2023-06-16 20:09:39 -0300

    pequeños cambios

---
+++ nuevas practicas/coercion.js
@@ -0,0 +1,14 @@
+var a = String(2);
+var b = Number("2");
+var c = Boolean(0);
+var d = Boolean(1);
+var e = Boolean(-1);
+var f = Boolean("hola");
+var g = tipeof a;
+var h = tipeof b;
+console.log (g);
+console.log (h);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
---
+++ nuevas practicas/hoisting.js
@@ -0,0 +1,5 @@
+var miNombre;
+miNombre = "Diego";
+//esto pasa por que js detecta las funciones y las variables y las pone en memoria antes de que las mismas tengan valor
+//esto se llama hoisting
+//esto no pasa con las variables que se declaran con let
