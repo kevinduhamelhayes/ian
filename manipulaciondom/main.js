abfdacaabcc8a90faf35d5cdc953e62b9e2a656a
Kevin Duhamel Hayes <kevinduhamelh@gmail.com>
2023-06-21 02:54:58 -0300

    arreglos

--- manipulaciondom/main.js
+++ manipulaciondom/main.js
@@ -1,3 +1,12 @@
+// para seleccionar en js un elemento del html se usa document.getElementById("id")
+// para seleccionar en js un elemento del html se usa document.querySelector("selector")
+// para seleccionar en js un elemento del html se usa document.querySelectorAll("selector")
+// para seleccionar en js un elemento del html se usa document.getElementsByClassName("clase")
+// para seleccionar en js un elemento del html se usa document.getElementsByTagName("etiqueta")
+// para seleccionar en js un elemento del html se usa document.getElementsByName("nombre")
+//
+
+
 document.getElementById("boton").addEventListener("click", cambiarTitulo);
 
 function cambiarTitulo() {
@@ -5,3 +14,15 @@ function cambiarTitulo() {
   var titulo = document.getElementById("titulo");
   titulo.innerHTML = nuevoTitulo;
 }
+
+const h1=document.querySelector('h1');const p=document.querySelector('p');
+const parrafito=document.getElementsByClassName('parrafito');
+const pid=document.getElementById('pid');const input=document.querySelector('input');
+console.log(input.value);console.log({h1,p,parrafito,pid,input,});
+h1.innerHTML='Patito <br> Feo';h1.innerText='Patito <br> Feo';
+h1.classList.add('rojo');h1.classList.remove('verde');input.value="456"
+const img=document.createElement('img');
+img.setAttribute('src','https://static.platzi.com/static/images/conf/logo_black@2x.png');
+console.log(img);
+pid.innerHTML="";
+pid.appendChild(img);
